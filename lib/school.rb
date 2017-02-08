e9990afab77fe730a8575a709680f65f2cc9502f
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-02-08 10:55:05 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
+require 'pry'
+
 # code here!
+class School
+
+	attr_reader :roster
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(name,grade)
+		if @roster.keys.include?(grade)
+			@roster[grade] << name
+		else
+			@roster[grade] = [].push(name)
+		end
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		Hash[@roster.each do |key,value|
+			@roster[key] = value.sort
+		end.sort]
+	end
+
+
+end
